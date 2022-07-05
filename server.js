6eb4fd5db6bdad23bdae1684c8681e02a8f89939
NalbertC <nalberthcastro1510@mail.com>
2022-07-05 10:19:49 -0300

    renderizando js

--- server.js
+++ server.js
@@ -6,7 +6,38 @@ app.set("view engine", "ejs")
 
 
 app.get("/", function (req, res) {
-    res.render("pages/index")
+    const itens =[
+        {
+            title: "D",
+            message: "esemvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa Javascript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "lto nível"
+        },
+        {
+            title: "I",
+            message: "nstall ejs"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        },
+
+    ]
+    const subtitle = "Uma linguagem de modelagem para criação de página HTML utilizando JavaScript"
+    res.render("pages/index", {
+        qualitys: itens,
+        subtitle: subtitle
+    })
 })
 
 app.get("/sobre", function (req, res) {
